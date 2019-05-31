dd8a75cbc656a407e3a7c187e5169454b352acf0
Achmad Akbar Megantara <48613329+achmegantara@users.noreply.github.com>
2019-05-31 15:26:00 +0700

    Add files via upload

---
+++ ledkedapkedip.ino
@@ -0,0 +1,18 @@
+int led = 9;
+int led2 = 12;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(led, OUTPUT);
+pinMode(led2, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    digitalWrite(led, HIGH);
+    delay(1000);
+    digitalWrite(led, LOW);
+    digitalWrite(led2, HIGH);
+    delay(1000);
+    digitalWrite(led2, LOW);
+}
